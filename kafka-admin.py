695c5fbbda6d5897d35b772ca350b222d6822b2b
Jash Lee <s905060@gmail.com>
2018-02-17 02:46:38 -0500

    Add new features
    Add revoke-leadership && rebalance-leadership support

--- kafka-admin.py
+++ kafka-admin.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
 # Simple Kafka admin tool with minimal data movement
 # Author: Jash Lee
@@ -19,10 +19,11 @@ import random
 import argparse
 from kazoo.client import KazooClient
 
+
 class KafkaReassigner():
 
-    def __init__(self, zookeeper_server, file_path):
-        self.file_path = file_path
+    def __init__(self, zookeeper_server, json_file_path):
+        self.json_file_path = json_file_path
         self.zk = KazooClient(hosts=zookeeper_server, read_only=True)
         self.zk.start()
 
@@ -55,7 +56,7 @@ class KafkaReassigner():
         return self.zk_query_list(topic)
 
     def write_json_file(self, data):
-        with open(self.file_path, 'w') as fp:
+        with open(self.json_file_path, 'w') as fp:
             json.dump(data, fp, sort_keys=True, indent=4)
 
     def get_topic_replica(self, topic, partitions):
@@ -76,6 +77,55 @@ class KafkaReassigner():
         elif replica > 3:
             replica = 3
         return int(replica)
+
+    def rebalance_leadership(self, topics):
+        final_new_partition_list = []
+
+        if len(topics) == 0:
+            topics = self.get_topics_list()
+
+        for topic in topics:
+            data = self.get_topic_partitions_mapping(topic)
+            partitions = data['partitions']
+            replica = self.get_topic_replica(topic, partitions)
+            for partition_id, partition_list in partitions.iteritems():
+                partition_list = self.rebalancer(replica, partition_list)
+                new_partition_list = []
+                if len(partition_list) > 1:
+                    random.shuffle(partition_list)
+                    new_partition_list = partition_list
+                else:
+                    new_partition_list = partition_list
+                tmp_dict = {"topic": topic, "partition": int(partition_id), "replicas": new_partition_list}
+                final_new_partition_list.append(tmp_dict)
+
+        self.generate_json(final_new_partition_list)
+
+    def revokeleadership(self, topics, broker):
+        final_new_partition_list = []
+
+        if len(topics) == 0:
+            topics = self.get_topics_list()
+
+        for topic in topics:
+            data = self.get_topic_partitions_mapping(topic)
+            partitions = data['partitions']
+            replica = self.get_topic_replica(topic, partitions)
+            for partition_id, partition_list in partitions.iteritems():
+                partition_list = self.rebalancer(replica, partition_list)
+                new_partition_list = []
+                if len(partition_list) > 1:
+                    if int(broker) == partition_list[0]:
+                        partition_list[0], partition_list[-1] = partition_list[-1], partition_list[0]
+                        new_partition_list = partition_list
+                    else:
+                        new_partition_list = partition_list
+                else:
+                    new_partition_list = partition_list
+                tmp_dict = {"topic": topic, "partition": int(partition_id), "replicas": new_partition_list}
+                final_new_partition_list.append(tmp_dict)
+
+        self.generate_json(final_new_partition_list)
 
     def decommission(self, topics, decommission_broker_list):
         newbrokerlist = [int(x) for x in self.get_alive_broker_list() if x not in decommission_broker_list]
@@ -138,7 +188,7 @@ class KafkaReassigner():
             if (alive_brokers_count - len(recommission_broker_list)) >= partition_count:
                 for partition_id, partition_list in partitions.iteritems():
                     partition_list = self.rebalancer(replica, partition_list)
-                    partition_list = [int(partition) for partition in partition_list ]
+                    partition_list = [int(partition) for partition in partition_list]
                     tmp_dict = {"topic": topic, "partition": int(partition_id), "replicas": partition_list}
                     final_new_partition_list.append(tmp_dict)
             else:
@@ -179,7 +229,8 @@ class KafkaReassigner():
                     replica = int(random.choice(newbrokerlist))
                 new_replica_list.append(replica)
             partition_list += new_replica_list
-            return random.shuffle(partition_list)
+            random.shuffle(partition_list)
+            return partition_list
 
         elif len(partition_list) > replica:
             # replica_range will be negative number
@@ -192,7 +243,7 @@ class KafkaReassigner():
         new_partition_json = {"version": 1, "partitions": final_new_partition_list}
         print new_partition_json
         self.write_json_file(new_partition_json)
-        print 'Exported to ' + self.file_path
+        print 'Exported to ' + self.json_file_path
         self.zk.stop()
 
     def set_replication_factor(self, topics, new_replica):
@@ -207,7 +258,7 @@ class KafkaReassigner():
 
             for partition_id, partition_list in partitions.iteritems():
                 partition_list = self.rebalancer(replica, partition_list)
-                tmp_dict = {"topic": topic, "partition": int(partition_id) , "replicas": partition_list}
+                tmp_dict = {"topic": topic, "partition": int(partition_id), "replicas": partition_list}
                 final_new_partition_list.append(tmp_dict)
 
         self.generate_json(final_new_partition_list)
@@ -216,10 +267,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple Kafka Decommission/Recommission tool')
     parser.add_argument("-z", "--zookeeper", type=str, help="Zookeeper server hostname:port", required=True)
     parser.add_argument("-t", "--topics", type=str, help="Topics comma-separated list, will ressign all topics if not specifying any topics", default="")
-    parser.add_argument("-m", "--mode", type=str, choices=["decommission", "recommission", "set-replica", "print-topic-list"], help="Mode", required=True)
+    parser.add_argument("-m", "--mode", type=str, choices=["decommission", "recommission", "revoke-leadership", "rebalance-leadership", "set-replica", "print-topic-list"], help="Mode", required=True)
     parser.add_argument("-r", "--replica", type=str, help="Set Replication Factor")
     parser.add_argument("-b", "--brokers", type=str, help="Brokers ID comma-separated list")
-    parser.add_argument("-f", "--file_path", type=str, help="Output JSON file path", default="/tmp/afka-admin-output.json")
+    parser.add_argument("-f", "--json_file_path", type=str, help="Output JSON file path", default="/tmp/kafka-admin-output.json")
     args = parser.parse_args()
 
     zookeeper_server = args.zookeeper
@@ -228,7 +279,7 @@ if __name__ == '__main__':
     else:
         topics = args.topics
 
-    file_path = args.file_path
+    json_file_path = args.json_file_path
     mode = args.mode
 
     if mode == 'decommission' or mode == 'recommission':
@@ -236,17 +287,29 @@ if __name__ == '__main__':
             brokers = args.brokers.split(',')
         except:
             raise ValueError("Missing broker IDs")
-        kafkareassigner = KafkaReassigner(zookeeper_server, file_path)
+        kafkareassigner = KafkaReassigner(zookeeper_server, json_file_path)
         if mode == 'decommission':
             kafkareassigner.decommission(topics, brokers)
         elif mode == 'recommission':
             kafkareassigner.recommission(topics, brokers)
     elif mode == 'set-replica':
         replica = int(args.replica)
-        if replica is None or (replica > 3 or replica < 1) :
+        if replica is None or (replica > 3 or replica < 1):
             raise ValueError("Missing replica parameter or replica greater than 3")
-        kafkareassigner = KafkaReassigner(zookeeper_server, file_path)
+        kafkareassigner = KafkaReassigner(zookeeper_server, json_file_path)
         kafkareassigner.set_replication_factor(topics, replica)
+    elif mode == 'revoke-leadership':
+        try:
+            brokers = args.brokers.split(',')
+            broker = brokers[0]
+            print 'Revoking leadership from broker: %s only' % broker
+        except:
+            raise ValueError("Missing broker IDs")
+        kafkareassigner = KafkaReassigner(zookeeper_server, json_file_path)
+        kafkareassigner.revokeleadership(topics, broker)
+    elif mode == 'rebalance-leadership':
+        kafkareassigner = KafkaReassigner(zookeeper_server, json_file_path)
+        kafkareassigner.rebalance_leadership(topics)
     elif mode == 'print-topic-list':
-        kafkareassigner = KafkaReassigner(zookeeper_server, file_path)
+        kafkareassigner = KafkaReassigner(zookeeper_server, json_file_path)
         print kafkareassigner.print_topics_list()
